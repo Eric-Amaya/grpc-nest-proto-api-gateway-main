5d6ad974df339b5753cb0de96a5237a04eb9216a
Eric-Amaya <eric.amaya@alumnos.ucn.cl>
2024-06-13 23:24:08 -0400

    Update of every endpoints from the order ms

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Inject } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
-import { OrderServiceClient, ORDER_SERVICE_NAME, CreateOrderRequest, GetOrderRequest, GetAllOrdersRequest } from './order.pb';
+import { OrderServiceClient, ORDER_SERVICE_NAME, CreateOrderRequest, GetOrderRequest, GetAllOrdersRequest, CreateTableRequest, GetTablesByNameRequest, GetAllTablesRequest, UpdateTableStateRequest, CreateSaleRequest, GetAllSalesRequest, GetSalesByUserRequest, GetSalesByDateRequest } from './order.pb';
 import { Observable } from 'rxjs';
 import { Body, Get, Param, Post } from '@nestjs/common';
 import { Auth } from '../common/decorators/auth.decorator';
@@ -17,7 +17,31 @@ export class OrderController {
     this.orderService = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
   }
 
-  @Post()
+  @Post('tables/create')
+  @Auth(Role.USER)
+  createTable(@Body() request:  CreateTableRequest): Observable<any> {
+    return this.orderService.createTable(request);
+  }
+
+  @Get('tables/name')
+  @Auth(Role.USER)
+  getTablesByName(@Body() request:  GetTablesByNameRequest): Observable<any> {
+    return this.orderService.getTablesByName(request);
+  }
+
+  @Get('tables')
+  @Auth(Role.USER)
+  getAllTables(@Body() request:  GetAllTablesRequest): Observable<any> {
+    return this.orderService.getAllTables(request);
+  }
+
+  @Post('tables/update')
+  @Auth(Role.USER)
+  updateTableState(@Body() request:  UpdateTableStateRequest): Observable<any> {
+    return this.orderService.updateTableState(request);
+  }
+
+  @Post('create')
   @Auth(Role.USER)
   createOrder(@Body() request: CreateOrderRequest): Observable<any> {
     return this.orderService.createOrder(request);
@@ -35,5 +59,29 @@ export class OrderController {
   getAllOrders(): Observable<any> {
     const request: GetAllOrdersRequest = {};
     return this.orderService.getAllOrders(request);
+  }
+
+  @Post('sales/create')
+  @Auth(Role.USER)
+  createSale(@Body() request: CreateSaleRequest): Observable <any> {
+    return this.orderService.createSale(request);
+  }
+
+  @Get('sales')
+  @Auth(Role.USER)
+  getAllSales(@Body() request: GetAllSalesRequest): Observable <any> {
+    return this.orderService.getAllSales(request);
+  }
+
+  @Get('sales/user')
+  @Auth(Role.USER)
+  getSalesByUser(@Body() request: GetSalesByUserRequest): Observable <any> {
+    return this.orderService.getSalesByUser(request);
+  }
+
+  @Get('sales/date')
+  @Auth(Role.USER)
+  getSalesByDate(@Body() request: GetSalesByDateRequest): Observable <any> {
+    return this.orderService.getSalesByDate(request);
   }
 }
